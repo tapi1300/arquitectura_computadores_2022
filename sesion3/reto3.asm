a0f845d3016f5ee81792e5fc6247aab77d24c38c
David Tapiador <d.tapiador.2018@alumnos.urjc.es>
2022-02-17 21:31:00 +0100

    Avances en sesion3

---
+++ sesion3/reto3.asm
@@ -0,0 +1,11 @@
+#-- David Tapiador
+
+#-- Programa para probar la definición de variables
+
+	.data
+str:	.asciz "Sesion 3 Ejercicio 3"
+v1:	.word 0x12345678
+v2:	.word 0x11223344
+v3:	.word 0xCACABACA
+v4:	.word 0x00FABADA
+
